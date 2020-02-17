7ff742f730e4ed4afcbd1a3e40558866b7127a8a
Ari Paasonen <paasar@gmail.com>
2020-02-17 13:29:48 +1000

    More realistic rotation times

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-// add styles
 import './style.css'
-// three.js
 import * as T from 'three'
 
-import * as OrbitControls from 'three-orbitcontrols';
+import * as OrbitControls from 'three-orbitcontrols'
 
-import * as C from './controls'
+import * as C from './constants'
+import * as CONTROLS from './controls'
 import * as M from './model'
 
 let previousNow = Date.now()
-let currentTick = previousNow
 let pause = false
 
-C.listenInput({togglePause: () => { pause = !pause}})
+CONTROLS.listenInput({togglePause: () => { pause = !pause }})
 
 // create the scene
 let scene = new T.Scene()
@@ -27,7 +25,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 // add canvas to dom
 document.body.appendChild(renderer.domElement)
 
-const orbitControls = new OrbitControls(camera, renderer.domElement);
+const orbitControls = new OrbitControls(camera, renderer.domElement)
 
 // add axis to the scene
 let axis = new T.AxesHelper(10000)
@@ -36,8 +34,9 @@ scene.add(axis)
 const sunLight = new T.PointLight(0xffffff, 1.0)
 scene.add(sunLight)
 
-const ambience = new T.AmbientLight( 0x101010 ); // soft white light
-scene.add( ambience );
+// soft white light
+const ambience = new T.AmbientLight( 0x101010 )
+scene.add(ambience)
 
 // debug box
 const box = M.createBox()
@@ -55,51 +54,55 @@ const targetPlanet = model.earth.planet
 camera.lookAt(targetPlanet.position)
 orbitControls.target = targetPlanet.position
 
-let moveMoon = true
-
-function nextTick(speed: number) {
+function tick(): number {
     const now = Date.now()
     const timePassed = pause ? 0 : now - previousNow
     previousNow = now
 
-    const nextTick = currentTick + timePassed
-    currentTick = nextTick
-    return 0.001 * nextTick * speed
+    return timePassed
 }
 
+let moonPosition = {cos: 0, sin: 0}
+let moveMoon = true
+
+const speed = 38880 // 38880 = full moon orbit in one minute
+
 function advanceState(): void {
-    const speed = 1
-    const tick = nextTick(speed)
+    const timePassed = tick()
+
+    const passedQuotientOfHour = timePassed / C.HOUR_IN_MILLIS
 
     const earth = model.earth.planet
-    const moon = model.earth.satellites[0];
+    const moon = model.earth.satellites[0]
 
     if (moveMoon) {
-        //~27.322 d orbit & rotation
-        moon.position.copy(earth.position);
+        moon.position.copy(earth.position)
+        const moonOrbitRadians = rotation(C.MOON_ORBITS_IN_HOUR, passedQuotientOfHour)
+        moonPosition = {cos: moonPosition.cos - moonOrbitRadians,
+                        sin: moonPosition.sin - moonOrbitRadians}
         moon.position.set(
-            moon.position.x + Math.cos(-tick * 0.01) * 20,
+            moon.position.x + Math.cos(moonPosition.cos) * 20,
             moon.position.y,
-            moon.position.z + Math.sin(-tick * 0.01) * 20
+            moon.position.z + Math.sin(moonPosition.sin) * 20
         )
         moon.lookAt(earth.position)
         moon.rotateY(-1.2)
         //moveMoon = !moveMoon
     }
 
-    if (!pause) {
-        //TODO correct rotation speeds
-        const baseRotation = 0.005 * speed
-        model.sun.rotateY(baseRotation / 10)
-        model.mercury.planet.rotateY(baseRotation)
-        model.venus.planet.rotateY(baseRotation)
-        earth.rotateY(baseRotation)
-        model.mars.planet.rotateY(baseRotation)
-        model.jupiter.planet.rotateY(baseRotation / 5)
-        model.saturn.planet.rotateY(baseRotation / 5)
-        model.uranus.planet.rotateY(baseRotation)
-        model.neptune.planet.rotateY(baseRotation)
-    }
+    model.sun.rotateY(rotation(C.SUN_TURNS_IN_HOUR, passedQuotientOfHour))
+    model.mercury.planet.rotateY(rotation(C.MERCURY_TURNS_IN_HOUR, passedQuotientOfHour))
+    model.venus.planet.rotateY(rotation(C.VENUS_TURNS_IN_HOUR, passedQuotientOfHour))
+    earth.rotateY(rotation(C.EARTH_TURNS_IN_HOUR, passedQuotientOfHour))
+    model.mars.planet.rotateY(rotation(C.MARS_TURNS_IN_HOUR, passedQuotientOfHour))
+    model.jupiter.planet.rotateY(rotation(C.JUPITER_TURNS_IN_HOUR, passedQuotientOfHour))
+    model.saturn.planet.rotateY(rotation(C.SATURN_TURNS_IN_HOUR, passedQuotientOfHour))
+    model.uranus.planet.rotateY(rotation(C.URANUS_TURNS_IN_HOUR, passedQuotientOfHour))
+    model.neptune.planet.rotateY(rotation(C.NEPTUNE_TURNS_IN_HOUR, passedQuotientOfHour))
+}
+
+function rotation(radiansInHour: number, passedQuotientOfHour: number): number {
+    return radiansInHour * passedQuotientOfHour * speed
 }
 
 function animate(): void {
